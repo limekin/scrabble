97a311e89c357b86796b70637973222d8a6d11f6
kevintj <kevin.t.jayan@gmail.com>
2014-04-16 08:32:12 +0530

    Scrabble complete.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,5 +1,62 @@
 class Scrabble
   def self.score(word)
-    0
+
+	  ret = 0
+	  return ret if word.nil? or word.empty?
+
+	  word.each_char do |letter|
+		  ret += letterScore(letter)
+          end
+	  ret
+
   end
+
+  def self.highest_score_from(words)
+
+	  ret = ""
+	  maxScore = 0
+	  words.each do |word|
+		  wordScore = score(word)
+		  if maxScore < wordScore 
+			  maxScore = wordScore
+			  ret = word
+		  elsif maxScore == wordScore
+			  if bonus?(word) and not bonus?(ret)
+				  ret = word
+			  elsif word.length < ret.length
+				  ret = word
+		          end
+		  end
+
+   	  end
+	  ret
+  	  
+	  
+
+
+  end
+  
+
+  def self.bonus?(word)
+
+	  word.length == 7
+  end
+
+
+
+  def self.letterScore(letter)
+
+	  ret = -1
+	  letter = letter.upcase
+	  ret = 1 if %w( A E I O U L N R S T ).include? letter
+	  ret = 2 if %w( D G).include? letter
+	  ret = 3 if %w( B C M P  ).include? letter
+	  ret = 4 if %w( F H V W Y  ).include? letter
+	  ret = 5 if %w( K  ).include? letter
+	  ret = 6 if %w( J X  ).include? letter
+	  ret = 7  if %w( Q Z ).include? letter
+	  ret
+
+  end
+
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -6,5 +6,23 @@ describe Scrabble do
       expect( Scrabble.score("a") ).to eq 1
       expect( Scrabble.score("f") ).to eq 4
     end
+    
+    it "gets a valid score of a word" do
+    	expect( Scrabble.score("hello") ).to eq(8)
+        expect( Scrabble.score("") ).to eq(0)
+	expect( Scrabble.score(nil) ).to eq(0)
+    end
+
+
   end
+
+  describe ".highest_score_from" do
+	  it "gets the highest score" do
+		  expect( Scrabble.highest_score_from(['home', 'word', 'hello', 'sound'])).to eq("home")
+		  expect( Scrabble.highest_score_from(['hello', 'word', 'sound'])).to eq("word")
+		  expect( Scrabble.highest_score_from(['home', 'word', 'silence'])).to eq("silence")
+		  expect( Scrabble.highest_score_from(['hi', 'word', 'ward'])).to eq("word")
+          end
+  end
+	  
 end
